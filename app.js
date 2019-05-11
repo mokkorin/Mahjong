4be6a6de6275bf17f4f3a994fdf85ef28279e3cf
Knumber7 <qosmos7@gmail.com>
2019-05-11 16:25:42 +0900

    add express

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-var http = require('http'); //httpモジュール呼び出し
-var server = http.createServer(function (request, response) {
-    // リクエストを受けると以下のレスポンスを送信する
-    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
-    response.write('Hello World\n'); // レスポンスボディに「Hello World」を書き込み
-    response.end(); // レスポンス送信を完了する
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 });
-server.listen(process.env.PORT || 8080);  //8080番ポートで待ち受け
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
